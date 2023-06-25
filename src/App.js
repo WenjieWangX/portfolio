468b44c943cacef34704f0aca5ab99da86e1ea75
Wenjie Wang <jp1349785420@gmail.com>
2023-06-24 23:06:24 -0400

    updated initial files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import Background from "./components/Background";
-import Navigation from "./components/Navigation";
-import NavigationSocial from "./components/NavigationSocial";
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 const App = () => {
   return (
-    <div>
-      <Background />
-      <Navigation />
-      <NavigationSocial />
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
