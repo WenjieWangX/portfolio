321e5770866e164abe9d0f0a9ac990c41fc5e2ae
Wenjie Wang <jp1349785420@gmail.com>
2023-06-26 17:00:17 -0400

    about and hoc

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,6 @@
 import { Suspense, useEffect, useState } from "react"
 import { Canvas } from "@react-three/fiber"
-import { OrbitControls, PositionPoint, Preload, useGLTF } from "@react-three/drei"
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
 
 import CanvasLoader from "../Loader";
 
